458bc0bb2720e9964672d5bd4353c01e008b5af3
VadimAbyshev <boss.abyshev@gmail.com>
2023-09-01 15:48:00 +0500

    1

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/UnauthorizedError');
-const { SECRET_KEY } = process.env;
+const { SECRET_KEY = 'sobaka' } = process.env;
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
